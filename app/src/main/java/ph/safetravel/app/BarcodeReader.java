6882c8a3bce160c0de839ad0eae85db1e51967db
nctiglao@up.edu.ph <M@tthew633>
2020-04-22 15:45:20 +0800

    Initial commit

--- app/src/main/java/ph/safetravel/app/BarcodeReader.java
+++ app/src/main/java/ph/safetravel/app/BarcodeReader.java
@@ -1,27 +1,12 @@
 package ph.safetravel.app;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
-import android.app.Activity;
-import android.app.Dialog;
-import android.content.ActivityNotFoundException;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.vision.barcode.Barcode;
-import java.util.List;
 
 public class BarcodeReader extends AppCompatActivity  {
     public static TextView tvresult;
